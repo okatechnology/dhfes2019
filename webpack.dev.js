e10a220b103e59cbaad8c3edba339afa66589931
whatasoda <git@whatasoda.me>
2019-10-18 17:23:17 +0900

    fix: add JSON.stringify for DefinePlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: 'development',
+        NODE_ENV: JSON.stringify('development'),
       },
     }),
   ],
--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: 'development',
+        NODE_ENV: JSON.stringify('production'),
       },
     }),
     new TerserPlugin({
