efd4fd14705b440c8772bc0c62a12a2c437bf946
whatasoda <git@whatasoda.me>
2019-11-23 20:40:38 +0900

    feat(app): add supportedTagMap

---
+++ src/app/utils/supportedTagMap.ts
@@ -0,0 +1,15 @@
+export interface TagItem {
+  name: string;
+  color: string;
+}
+
+const createTagMap = <T extends Record<string, string>>(input: T) => {
+  return Object.entries(input).reduce<Record<string, TagItem>>((acc, [name, color]) => {
+    acc[name] = { name, color };
+    return acc;
+  }, {}) as Record<keyof T, TagItem>;
+};
+
+const TagMap = createTagMap({});
+
+export default TagMap;
