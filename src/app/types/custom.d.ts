5eb0fe3986edadaf06970e3975aca4d9e887c209
okatechnology <tonkotsu6218@gmail.com>
2019-11-25 20:24:13 +0900

    add: added EventItem type

--- src/app/types/custom.d.ts
+++ src/app/types/custom.d.ts
@@ -1,27 +1,25 @@
-declare module '*.jpg' {
-  const _path: string;
-  export default _path;
+import tagMap from '../utils/supportedTagMap';
+import placeMap from '../utils/supportedPlaceMap';
+
+declare global {
+  interface GlobalState {}
+
+  type NullableKey<T extends object> = {
+    [K in keyof T]-?: (null extends T[K] ? K : never) | (undefined extends T[K] ? K : never);
+  }[keyof T];
+
+  type PartialIfOptional<T extends object> = {
+    [K in keyof (Omit<T, NullableKey<T>> & { [K in NullableKey<T>]?: never })]: T[K];
+  };
+
+  interface MapRect extends Pick<ClientRect, 'bottom' | 'left' | 'right' | 'top'> {}
+  interface MapPoint extends Pick<MapRect, 'top' | 'left'> {}
+
+  interface EventItem {
+    name: string;
+    description: string;
+    tag: Array<keyof typeof tagMap>;
+    image: string;
+    place: keyof typeof placeMap;
+  }
 }
-
-declare module '*.png' {
-  const _path: string;
-  export default _path;
-}
-
-declare module '*.svg' {
-  const _path: string;
-  export default _path;
-}
-
-interface GlobalState {}
-
-type NullableKey<T extends object> = {
-  [K in keyof T]-?: (null extends T[K] ? K : never) | (undefined extends T[K] ? K : never);
-}[keyof T];
-
-type PartialIfOptional<T extends object> = {
-  [K in keyof (Omit<T, NullableKey<T>> & { [K in NullableKey<T>]?: never })]: T[K];
-};
-
-interface MapRect extends Pick<ClientRect, 'bottom' | 'left' | 'right' | 'top'> {}
-interface MapPoint extends Pick<MapRect, 'top' | 'left'> {}
