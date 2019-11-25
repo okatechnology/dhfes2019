c40c80b4fd34c43694cc7628cf5ee3104630611d
okatechnology <tonkotsu6218@gmail.com>
2019-11-25 20:24:28 +0900

    add: module type file

---
+++ src/app/types/module.d.ts
@@ -0,0 +1,14 @@
+declare module '*.jpg' {
+  const _path: string;
+  export default _path;
+}
+
+declare module '*.png' {
+  const _path: string;
+  export default _path;
+}
+
+declare module '*.svg' {
+  const _path: string;
+  export default _path;
+}
