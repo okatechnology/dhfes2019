ea905f1f342e9b549b0fe83a6d264b7c5d19ecbc
whatasoda <git@whatasoda.me>
2019-11-27 22:08:24 +0900

    chore: add unit

--- src/app/utils/units.ts
+++ src/app/utils/units.ts
@@ -2,3 +2,4 @@ export const px = (value: string | number) => `${value}px`;
 export const pct = (value: string | number) => `${value}%`;
 export const vh = (value: string | number) => `${value}vh`;
 export const vw = (value: string | number) => `${value}vw`;
+export const rem = (value: string | number) => `${value}rem`;
